534f7b9ed000767ce602307d449a4995edd15012
Rozina <rozina@Aamirs-MBP.broadband>
2022-06-09 20:33:42 +0100

    weather pics

--- src/App.js
+++ src/App.js
@@ -1,23 +1,44 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import { useState } from "react";
+import { dict } from "./Weathercode";
+import { Card } from "react-bootstrap";
+import { Button } from "react-bootstrap";
 
 function App() {
+  const [temperature, setTemperature] = useState();
+  const [weatherCode, setWeatherCode] = useState();
+  const [card, setCard] = useState();
+  const url =
+    "https://api.open-meteo.com/v1/forecast?latitude=51.5002&longitude=-0.1262&current_weather=true&timezone=Europe%2FLondon";
+
+  fetch(url)
+    .then((data) => data.json())
+    .then((res) => {
+      setTemperature(res.current_weather.temperature);
+      setWeatherCode(res.current_weather.weathercode);
+      console.log(
+        res.current_weather.temperature,
+        res.current_weather.weathercode
+      );
+    });
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Weather</h1>
+      <Card style={{ width: "18rem" }}>
+        <Card.Img variant="top"  src={dict[weatherCode]?.image.url} alt={dict[weatherCode].image.alt} />
+        <Card.Body>
+          <Card.Title>Forecast</Card.Title>
+          <Card.Text>
+            search forecast
+          </Card.Text>
+          <Button variant="primary">Go somewhere</Button>
+        </Card.Body>
+      </Card>
+      <p>Temperature:{temperature} </p>
+      <p>Code:{dict[weatherCode]?.weather}</p>
+      <footer>Created by Rozina Hussain</footer>
     </div>
   );
 }
---
+++ src/Weathercode.js
@@ -0,0 +1,235 @@
+import React from "react";
+
+export const dict = {
+  0: {
+    weather: "Clear Sky",
+    image: {
+      url: "https://media.istockphoto.com/photos/beautiful-sky-with-white-cloud-background-picture-id517897328?b=1&k=20&m=517897328&s=170667a&w=0&h=QpeJ9U8Kdx7Wbj96k_8pYR_5NvM2FfcufYg2NJGqPUo=",
+      alt: "Clear sky",
+    },
+  },
+  1: {
+    weather: "Mainly clear",
+    image:
+      {
+      url: "https://images.unsplash.com/photo-1591797879399-db2ba1b500a7?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8N3x8Y2xvdWR5JTIwY2xlYXIlMjBza2llc3xlbnwwfHwwfHw%3D&auto=format&fit=crop&w=800&q=60",
+      alt: "Mainly clear",
+    },
+  },
+  2: {
+    weather: "Partly cloudy",
+    image:
+      {
+      url: "https://images.unsplash.com/photo-1649434881843-50961665a3aa?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTZ8fGNsb3VkeSUyMGNsZWFyJTIwc2tpZXN8ZW58MHx8MHx8&auto=format&fit=crop&w=800&q=60",
+      alt: "Partly cloudy",
+    },
+  },
+  3: {
+    weather: "Overcast",
+    image:
+      {
+      url: "https://images.unsplash.com/photo-1520934116-55b3399a0e81?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1180&q=80",
+      alt: "Overcast",
+    },
+  },
+  45: {
+    weather: "Fog",
+    image:
+      {
+      url: "https://images.unsplash.com/photo-1485236715568-ddc5ee6ca227?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8Zm9nfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=800&q=60",
+      alt: "Fog",
+    },
+  },
+  48: {
+    weather: "Depositing rime fog",
+    image:
+      {
+      url: "https://images.unsplash.com/photo-1485236715568-ddc5ee6ca227?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8Zm9nfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=800&q=60",
+      alt: "Depositing rime fog",
+    },
+  },
+  51: {
+    weather: "Light Drizzle",
+    image:
+      {
+      url: "https://images.unsplash.com/photo-1420496854436-ae64eb61e937?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxjb2xsZWN0aW9uLXBhZ2V8Nnw0MzcyMDkwfHxlbnwwfHx8fA%3D%3D&auto=format&fit=crop&w=800&q=60",
+      alt: "Light drizzle",
+    },
+  },
+  53: {
+    weather: "Moderate Drizzle",
+    image:
+      {
+      url: "https://images.unsplash.com/photo-1420496854436-ae64eb61e937?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxjb2xsZWN0aW9uLXBhZ2V8Nnw0MzcyMDkwfHxlbnwwfHx8fA%3D%3D&auto=format&fit=crop&w=800&q=60",
+      alt: "Moderate drizzle",
+    },
+  },
+  55: {
+    weather: "Dense intensity",
+    image:
+      {
+      url: "https://media.istockphoto.com/photos/rain-is-falling-in-a-puddle-in-the-garden-picture-id1292942434?b=1&k=20&m=1292942434&s=170667a&w=0&h=dAypvtzYHLZ2QSUNSWASEWvUwDNg9Sso3yb9QfByDks=",
+      alt: "Dense intensity",
+    },
+  },
+  56: {
+    weather: "Light Freezing Drizzle",
+    image:
+      {
+      url: "https://media.istockphoto.com/photos/after-a-freezing-rain-storm-picture-id172478633?b=1&k=20&m=172478633&s=170667a&w=0&h=sicLeFfJ9Szz3q1GuHOXznEDrD-i_0jXvIRY__PIzVY=",
+      alt: "Light freezing drizzle",
+    },
+  },
+  57: {
+    weather: "Dense Intensity Drizzle",
+    image:
+      {
+      url: "https://media.istockphoto.com/photos/after-a-freezing-rain-storm-picture-id172478633?b=1&k=20&m=172478633&s=170667a&w=0&h=sicLeFfJ9Szz3q1GuHOXznEDrD-i_0jXvIRY__PIzVY=",
+      alt: "Dense intensity drizzle",
+    },
+  },
+  61: {
+    weather: "light Rain Intensity",
+    image:
+      {
+      url: "https://images.unsplash.com/photo-1496034663057-6245f11be793?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MjB8fHJhaW58ZW58MHx8MHx8&auto=format&fit=crop&w=800&q=60",
+      alt: "Light rain intensity",
+    },
+  },
+  63: {
+    weather: "Moderate Rain Intensity",
+    image:
+      {
+      url: "https://media.istockphoto.com/photos/transparent-umbrella-under-rain-against-water-drops-splash-background-picture-id1257951336?k=20&m=1257951336&s=612x612&w=0&h=KMMSYxYzjtqg7NAvNTO8ahMz84J7QW0FjvMSZ12Bq6I=",
+      alt: "Moderate rain intensity",
+    },
+  },
+  65: {
+    weather: "Heavy Rain Intensity",
+    image:
+      {
+      url: "https://media.istockphoto.com/photos/rain-falling-background-water-splash-picture-id1048136518?b=1&k=20&m=1048136518&s=170667a&w=0&h=FO3peJKFggzDJunkd-qwYe3nDLKqt8d56LPCs7D6JLo=",
+      alt: "Heavy rain intensity",
+    },
+  },
+  66: {
+    weather: "Freezing Rain Light Intensity",
+    image:
+      {
+      url: "https://media.istockphoto.com/photos/frozen-tree-covered-in-icicles-picture-id530354119?b=1&k=20&m=530354119&s=170667a&w=0&h=QWkkf4pX0ur29mcZjHOzugspiHoiCvcouuuOh7jlCwc=",
+      alt: "Freezina rain light intensity",
+    },
+  },
+  67: {
+    weather: "Freezing Rain Heavy Intensity",
+    image:
+      {
+      url: "https://media.istockphoto.com/photos/frozen-tree-covered-in-icicles-picture-id530354119?b=1&k=20&m=530354119&s=170667a&w=0&h=QWkkf4pX0ur29mcZjHOzugspiHoiCvcouuuOh7jlCwc=",
+      alt: "Freezing rain heavy intensity",
+    },
+  },
+  71: {
+    weather: "Slight Snow Fall Intensity",
+    image:
+      {
+      url: "https://images.unsplash.com/photo-1575060694482-6a6a49458d38?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NHx8c2xlZXR8ZW58MHx8MHx8&auto=format&fit=crop&w=800&q=60",
+      alt: "Slight snow fall intensity",
+    },
+  },
+  73: {
+    weather: "Moderate Snow Fall Intensity",
+    image:
+      {
+      url: "https://images.unsplash.com/photo-1510798831971-661eb04b3739?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8M3x8bGlnaHQlMjBzbm93fGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=800&q=60",
+      alt: "Moderate snow fall intensity",
+    },
+  },
+  75: {
+    weather: "Heavy Snow Fall Intensity",
+    image:
+      {
+      url: "https://images.unsplash.com/photo-1516820208784-270b250306e3?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8aGVhdnklMjBzbm93JTIwZmFsbHxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=800&q=60",
+      alt: "Heavy snow fall intensity",
+    },
+  },
+  77: {
+    weather: "Snow Grains",
+    image:
+      {
+      url: "https://media.istockphoto.com/photos/disused-grain-silos-picture-id1299289272?b=1&k=20&m=1299289272&s=170667a&w=0&h=xkgWdcyFOm8mAyIWm3xMY7HakIZqy4MHtc8IiSLabGA=",
+      alt: "Snow grains",
+    },
+  },
+  80: {
+    weather: "Slight Rain Showers",
+    image:
+      {
+      url: "https://media.istockphoto.com/photos/rainy-day-picture-id1352385683?b=1&k=20&m=1352385683&s=170667a&w=0&h=RdYy8BXaDjVAHEjOPVRhlRg8mZwrmA8TVAhMZb_-Lbo=",
+      alt: "Slight rain showers",
+    },
+  },
+  81: {
+    weather: "Moderate Rain Showers",
+    image:
+      {
+      url: "https://media.istockphoto.com/photos/rainy-day-picture-id1352385683?b=1&k=20&m=1352385683&s=170667a&w=0&h=RdYy8BXaDjVAHEjOPVRhlRg8mZwrmA8TVAhMZb_-Lbo=",
+      alt: "Moderate rain showers",
+    },
+  },
+  82: {
+    weather: "Violent Rain Showers",
+    image:
+      {
+      url: "https://media.istockphoto.com/photos/rain-falling-background-water-splash-picture-id1048136518?b=1&k=20&m=1048136518&s=170667a&w=0&h=FO3peJKFggzDJunkd-qwYe3nDLKqt8d56LPCs7D6JLo=",
+      alt: "Violent rain showers",
+    },
+  },
+  85: {
+    weather: "Light Snow Showers",
+    image:
+      {
+      url: "https://media.istockphoto.com/photos/dangerous-driving-xxxl-picture-id157562045?b=1&k=20&m=157562045&s=170667a&w=0&h=JApnwNTUfHixqXwDXYtGSUFE2fcua-_uTM94oTHmo2E=",
+      alt: "Light snow showers",
+    },
+  },
+  86: {
+    weather: "Heavy Snow Showers",
+    image:
+      {
+      url: "https://images.unsplash.com/photo-1477601263568-180e2c6d046e?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8aGVhdnklMjBzbm93JTIwc2hvd2Vyc3xlbnwwfHwwfHw%3D&auto=format&fit=crop&w=800&q=60",
+      alt: "Heavy snow showers",
+    },
+  },
+  95: {
+    weather: "Slight Thunderstorm",
+    image:
+      {
+      url: "https://images.unsplash.com/photo-1594156596782-656c93e4d504?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8c2xpZ2h0JTIwdGh1bmRlcnN0b3JtfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=800&q=60",
+      alt: "Slight thunderstorm",
+    },
+  },
+  97: {
+    weather: "Moderate Thunderstorm",
+    image:
+      {
+      url: "https://images.unsplash.com/photo-1594156596782-656c93e4d504?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8c2xpZ2h0JTIwdGh1bmRlcnN0b3JtfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=800&q=60",
+      alt: "Moderate thunderstorm",
+    },
+  },
+  96: {
+    weather: "Slight Hail Thunderstorm",
+    image:
+      {
+      url: "https://media.istockphoto.com/photos/hail-picture-id466990201?k=20&m=466990201&s=612x612&w=0&h=D0WZUp4f8BzvUnA5rsom6iLjtZqEWa8UobEI0OKrCvY=",
+      alt: "Slight hail thunderstorm",
+    },
+  },
+  99: {
+    weather: "Heavy Hail Thunderstorm}",
+    image:
+      {
+      url: "https://media.istockphoto.com/photos/hail-picture-id466990201?k=20&m=466990201&s=612x612&w=0&h=D0WZUp4f8BzvUnA5rsom6iLjtZqEWa8UobEI0OKrCvY=",
+      alt: "Heavy hail thunderstorm",
+    },
+  },
+};
