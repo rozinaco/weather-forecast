8c83f804bb63081ea6463759d3598801bede86b1
Rozina <rozina@Aamirs-MBP.broadband>
2022-06-16 22:49:41 +0100

    added cit url and fetch datajson longitude latitude

--- src/App.js
+++ src/App.js
@@ -5,50 +5,74 @@ import { dict } from "./Weathercode";
 import { Card } from "react-bootstrap";
 import { Button } from "react-bootstrap";
 
-
 function App() {
   const [temperature, setTemperature] = useState();
   const [weatherCode, setWeatherCode] = useState();
-  const [card, setCard] = useState();
-  const url =
+  const [search, setSearch] = useState()
+
+  const urlForecast =
     "https://api.open-meteo.com/v1/forecast?latitude=51.5002&longitude=-0.1262&current_weather=true&timezone=Europe%2FLondon";
 
-  fetch(url)
-    .then((data) => data.json())
-    .then((res) => {
-      setTemperature(res.current_weather.temperature);
-      setWeatherCode(res.current_weather.weathercode);
-      console.log(
-        res.current_weather.temperature,
-        res.current_weather.weathercode
-      );
-    });
- 
+  function getWeather() {
+    const urlCity = "https://geocoding-api.open-meteo.com/v1/search?name="+search;
+    fetch(urlCity)
+      .then((data) => data.json())
+      .then((jsonData)=>{
+const longitude = jsonData.results[0].longitude
+const latitude = jsonData.results[0].latitude
+
+        
+      })
+
+
+      // .then((res) => {
+      //   setTemperature(res.current_weather.temperature);
+      //   setWeatherCode(res.current_weather.weathercode);
+      //   console.log(
+      //     res.current_weather.temperature,
+      //     res.current_weather.weathercode
+      //   );
+      // });
+  }
+
   return (
     <div className="App">
       <div className="clouds">
-	<div className="cloud x1"></div>
-	<div className="cloud x2"></div>
-	<div className="cloud x3"></div>
-	<div className="cloud x4"></div>
-	<div className="cloud x5"></div>
-</div>
+        <div className="cloud x1"></div>
+        <div className="cloud x2"></div>
+        <div className="cloud x3"></div>
+        <div className="cloud x4"></div>
+        <div className="cloud x5"></div>
+      </div>
 
       <div className="forecast">
-        <img className="img"
+        <img
+          className="img"
           src={dict[weatherCode]?.image.url}
           alt={dict[weatherCode]?.image.alt}
         />
         <div>
-          <Card.Title style={{fontSize: "50px", fontFamily: "Arial, Helvetica, sans-serif"}}>{dict[weatherCode]?.weather}</Card.Title>
+          <Card.Title
+            style={{
+              fontSize: "50px",
+              fontFamily: "Arial, Helvetica, sans-serif",
+            }}
+          >
+            {dict[weatherCode]?.weather}
+          </Card.Title>
           <Card.Text className="temp">{temperature} ºC </Card.Text>
         </div>
       </div>
-      <form className="form">
-      <input id="Search" type="text" class="input" placeholder="Search city here..." />
-      <button id="clear" class="go">Go</button>
-      </form>
-              
+      <div className="form">
+        <input onChange={e=>setSearch(e.target.value)}
+          id="Search"
+          type="text"
+          placeholder="Search city here..."
+        />
+        <button id="clear" onClick={getWeather}>
+          Go
+        </button>
+      </div>
 
       <footer>Created by Rozina Hussain</footer>
     </div>
